f97e2752786fe64bd630d95e204e320a06a689cb
vicky.gluck <vicky.gluck@divcom.ch>
2021-05-05 10:52:47 +0200

    Initial commit

---
+++ src/router/routes.js
@@ -0,0 +1,20 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'PageExercice1', component: () => import('pages/Exercice1.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
